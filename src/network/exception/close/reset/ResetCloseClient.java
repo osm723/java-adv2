1bba783ae066ec16020ad859ee0cdbd06f99156f
Oh <osm723@gmail.com>
2025-05-12 21:50:53 +0900

    섹션 11. HTTP - 기본 이론 71. HTTP 메서드

---
+++ src/network/exception/close/reset/ResetCloseClient.java
@@ -0,0 +1,37 @@
+package network.exception.close.reset;
+
+import java.io.*;
+import java.net.Socket;
+import java.net.SocketException;
+
+import static util.MyLogger.log;
+
+public class ResetCloseClient {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Socket socket = new Socket("localhost", 12345);
+        log("소켓 연결: " + socket);
+        InputStream input = socket.getInputStream();
+        OutputStream output = socket.getOutputStream();
+
+        Thread.sleep(1000);
+
+        output.write(1);
+
+        Thread.sleep(1000);
+
+        try {
+            int read = input.read();
+            System.out.println("read = " + read);
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            output.write(1);
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/network/tcp/autocloseable/ResourceCloseMainV4.java
@@ -0,0 +1,34 @@
+package network.tcp.autocloseable;
+
+public class ResourceCloseMainV4 {
+
+    public static void main(String[] args) {
+        try {
+            logic();
+        } catch (CallException e) {
+            System.out.println("CallException 예외처리");
+
+            Throwable[] suppressed = e.getSuppressed();
+            for (Throwable throwable : suppressed) {
+                System.out.println("suppressed ex: " + throwable);
+            }
+
+            throw new RuntimeException(e);
+        } catch (CloseException e) {
+            System.out.println("CloseException 예외처리");
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void logic() throws CallException, CloseException {
+        try (ResourceV2 resourceV1 = new ResourceV2("resource1");
+             ResourceV2 resourceV2 = new ResourceV2("resource2")) {
+
+            resourceV1.call();
+            resourceV2.callEx();
+        } catch (CallException e) {
+            System.out.println("ex: " + e);
+            throw e;
+        }
+    }
+}
